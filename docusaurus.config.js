84273a269c826d4c905a4adc2e75452d2bbec6b8
Noinkin <87577662+Noinkin@users.noreply.github.com>
2022-10-15 20:04:14 +0100

    docs: soon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,90 +6,90 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+    title: 'Math',
+    tagline: 'The future of math in JavaScript',
+    url: 'https://pangea-studios.github.io/mathematicsjs/',
+    baseUrl: '/mathematicsjs/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'Pangea-Studios', // Usually your GitHub org/user name.
+    projectName: 'Math', // Usually your repo name.
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/pangea-studios/math/tree/gh-pages',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'Math',
-        logo: {
-          alt: 'Math Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Docs',
-			},
-		  {
-            type: 'doc',
-            docId: 'guide/getting-started',
-            position: 'left',
-            label: 'Guide',
-			},
-		  {
-            type: 'doc',
-            docId: 'reference/home',
-            position: 'left',
-            label: 'Reference',
-          },
-          {
-            href: 'https://github.com/pangea-studios/mathematicsjs',
-            label: 'GitHub',
-            position: 'right',
-          },
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/pangea-studios/math/tree/gh-pages',
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
         ],
-      },
-      footer: {
-        style: 'dark',
-        copyright: `Copyright © ${new Date().getFullYear()} Pangea Studios.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+    ],
+
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            navbar: {
+                title: 'Math',
+                logo: {
+                    alt: 'Math Logo',
+                    src: 'img/logo.svg',
+                },
+                items: [
+                    {
+                        type: 'doc',
+                        docId: 'intro',
+                        position: 'left',
+                        label: 'Docs',
+                    },
+                    {
+                        type: 'doc',
+                        docId: 'guide/getting-started',
+                        position: 'left',
+                        label: 'Guide',
+                    },
+                    {
+                        type: 'doc',
+                        docId: 'reference/home',
+                        position: 'left',
+                        label: 'Reference',
+                    },
+                    {
+                        href: 'https://github.com/pangea-studios/mathematicsjs',
+                        label: 'GitHub',
+                        position: 'right',
+                    },
+                ],
+            },
+            footer: {
+                style: 'dark',
+                copyright: `Copyright © ${new Date().getFullYear()} Pangea Studios.`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+        }),
 };
 
 module.exports = config;
