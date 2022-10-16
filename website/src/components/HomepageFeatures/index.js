176fdc2a09950643c37ebb07f9d8bed5724de14d
Noinkin <87577662+Noinkin@users.noreply.github.com>
2022-10-16 09:17:26 +0100

    docs: Homepage && sidebar

--- website/src/components/HomepageFeatures/index.js
+++ website/src/components/HomepageFeatures/index.js
@@ -5,7 +5,6 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
         Math was built from the ground up by the Pangea Studios team, with 0 dependencies, it makes it easy for you to do what you do best
@@ -14,7 +13,6 @@ const FeatureList = [
   },
   {
     title: 'Interlocking functions',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
         Our functions interlock with each other so that they can work together, for example, you can use Fractions in Indices.
@@ -22,22 +20,18 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Docs made with Docusaurus',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Simple to use',
     description: (
       <>
-        Docusaurus, which uses React under the hood, allows us to have up to date documenation, guides and reference, so you know what to do.
+        With all the complex functionality under the hood, it allows you to do everything you need, with ease.
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({title, description}) {
   return (
     <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
         <p>{description}</p>
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
@@ -15,11 +14,10 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
+          <div
+            className="button button--secondary button--lg">
             npm i @pangea-studios/math
-          </Link>
+          </div>
         </div>
       </div>
     </header>
