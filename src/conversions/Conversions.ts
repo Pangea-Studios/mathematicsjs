289054524c2f9ee773094446fd006063788ebb69
PokyBread <luksel09@gmail.com>
2023-05-31 16:03:47 +0100

    Update Conversions.ts

--- src/conversions/Conversions.ts
+++ src/conversions/Conversions.ts
@@ -1,101 +1,83 @@
 export class Conversions {
-	/**
-	 * Converts a temperature from Fahrenheit to Celsius.
-	 *
-	 * @param {number} fahrenheit - The temperature in Fahrenheit to be converted.
-	 * @return {number} The temperature in Celsius after the conversion.
-	 */
-	static FahrenheitToCelsius(fahrenheit: number): number {
-		return ((fahrenheit - 32) * 5) / 9;
-	}
+  /**
+   * Converts meters to other length units.
+   *
+   * @param {number} meters - The length in meters to convert.
+   * @return {object} An object containing the converted lengths in inches, feet, yards, miles, nautical miles and paces.
+   */
 
-	/**
-	 * Converts a temperature in Kelvin to Celsius.
-	 *
-	 * @param {number} kelvin - The temperature in Kelvin.
-	 * @return {number} The temperature in Celsius.
-	 */
-	static KelvinToCelsius(kelvin: number): number {
-		return kelvin - 273.15;
-	}
+  static metersToOtherLengths(meters: number): object {
+    const inches = meters * 39.3701;
+    const feet = meters * 3.28084;
+    const yards = meters * 1.09361;
+    const miles = meters / 1609.344;
+    const nauticalMiles = meters / 1852;
+    const paces = meters / 0.762;
+    return { inches, feet, yards, miles, nauticalMiles, paces };
+  }
 
-	/**
-	 * Converts a Celsius temperature value to its Fahrenheit equivalent.
-	 *
-	 * @param {number} celsius - The temperature value in Celsius to convert.
-	 * @return {number} The equivalent temperature value in Fahrenheit.
-	 */
-	static CelsiusToFahrenheit(celsius: number): number {
-		return (celsius * 9) / 5 + 32;
-	}
+  /**
+   * Converts inches to other length units.
+   *
+   * @param {number} inches - The length in inches to convert.
+   * @return {object} An object containing the converted lengths in meters, feet, yards, miles, nautical miles and paces.
+   */
+  static inchesToOtherLengths(inches: number): object {
+    const meters = inches / 39.3701;
+    const feet = inches / 12;
+    const yards = inches / 36;
+    const miles = inches / 63360;
+    const nauticalMiles = inches / 72913.386;
+    const paces = inches / 29.5;
+    return { meters, feet, yards, miles, nauticalMiles, paces };
+  }
 
-	/**
-	 * Converts a temperature from Kelvin to Fahrenheit.
-	 *
-	 * @param {number} kelvin - the temperature in Kelvin.
-	 * @return {number} the temperature in Fahrenheit.
-	 */
-	static KelvinToFahrenheit(kelvin: number): number {
-		return ((kelvin - 273.15) * 9) / 5 + 32;
-	}
+  /**
+   * Converts feet to other length units.
+   *
+   * @param {number} feet - The length in feet to convert.
+   * @return {object} An object containing the converted lengths in meters, inches, yards, miles, nautical miles and paces.
+   */
+  static feetToOtherLengths(feet: number): object {
+    const meters = feet / 3.28084;
+    const inches = feet * 12;
+    const yards = feet / 3;
+    const miles = feet / 5280;
+    const nauticalMiles = feet / 6076.12;
+    const paces = feet / 2.5;
+    return { meters, inches, yards, miles, nauticalMiles, paces };
+  }
 
-	/**
-	 * Converts a temperature value from Celsius to Kelvin.
-	 *
-	 * @param {number} celsius - The temperature value in Celsius to be converted.
-	 * @return {number} The temperature value in Kelvin.
-	 */
-	static CelsiusToKelvin(celsius: number): number {
-		return celsius + 273.15;
-	}
+  /**
+   * Converts yards to other length units.
+   *
+   * @param {number} yards - The length in yards to convert.
+   * @return {object} An object containing the converted lengths in meters, inches, feet, miles, nautical miles and paces.
+   */
+  static yardsToOtherLengths(yards: number): object {
+    const meters = yards / 1.09361;
+    const inches = yards * 36;
+    const feet = yards * 3;
+    const miles = yards / 1760;
+    const nauticalMiles = yards / 2025.372;
+    const paces = yards / 0.9144;
+    return { meters, inches, feet, miles, nauticalMiles, paces };
+  }
 
-	/**
-	 * Converts temperature in Fahrenheit to Kelvin.
-	 *
-	 * @param {number} fahrenheit - temperature in Fahrenheit to be converted
-	 * @return {number} temperature in Kelvin
-	 */
-	static FahrenheitToKelvin(fahrenheit: number): number {
-		return ((fahrenheit - 32) * 5) / 9 + 273.15;
-	}
+  /**
+   * Converts miles to other length units.
+   *
+   * @param {number} miles - The length in miles to convert.
+   * @return {object} An object containing the converted lengths in meters, inches, feet, yards, nautical miles and paces.
+   */
+  static milesToOtherLengths(miles: number): object {
+    const meters = miles * 1609.344;
+    const inches = miles * 63360;
+    const feet = miles * 5280;
+    const yards = miles * 1760;
+    const nauticalMiles = miles / 1.15078;
+    const paces = miles * 2112;
+    return { meters, inches, feet, yards, nauticalMiles, paces };
+  }
 
-	/**
-	 * Converts radians to degrees.
-	 *
-	 * @param {number} radians - The angle in radians to be converted.
-	 * @return {number} The angle in degrees.
-	 */
-	static RadiansToDegrees(radians: number): number {
-		return (radians * 180) / Math.PI;
-	}
-
-	/**
-	 * Converts an angle from gradians to degrees.
-	 *
-	 * @param {number} gradians - The angle in gradians to be converted.
-	 * @return {number} The angle in degrees after conversion.
-	 */
-	static GradiansToDegrees(gradians: number): number {
-		return (gradians * 180) / Math.PI;
-	}
-
-	/**
-	 * Converts degrees to radians.
-	 *
-	 * @param {number} degrees - The angle in degrees to be converted.
-	 * @return {number} The angle converted to radians.
-	 */
-	static DegreesToRadians(degrees: number): number {
-		return (degrees * Math.PI) / 180;
-	}
-
-	/**
-	 * Converts an angle from gradians to radians.
-	 *
-	 * @param {number} gradians - The angle in gradians to convert.
-	 * @return {number} The converted angle in radians.
-	 */
-	static GradiansToRadians(gradians: number): number {
-		return (gradians * Math.PI) / 200;
-	}
 }
