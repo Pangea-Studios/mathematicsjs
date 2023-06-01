ce1962c3af31903facf5b9593623c5f0396600fe
PokyBread <luksel09@gmail.com>
2023-06-01 17:38:06 +0100

    idk

--- src/conversions/Conversions.ts
+++ src/conversions/Conversions.ts
@@ -1,52 +1,82 @@
 export const temperatureEnum = {
-	Kelvin: "KELVIN",
-	Celsius: "CELSIUS",
-	Fahrenheit: "FAHRENHEIT"
+	Kelvin: 'KELVIN',
+	Celsius: 'CELSIUS',
+	Fahrenheit: 'FAHRENHEIT',
 } as const;
 
 export const lengthEnum = {
-	Millimeters: "MILLIMETERS",
-	Decimeters: "DECIMETERS",
-	Centimeters: "CENTIMETERS",
-	Meters: "METERS",
-	Kilometers: "KILOMETERS",
-	Megameters: "MEGAMETERS",
-	Micrometers: "MICROMETERS",
-	Nanometers: "NANOMETERS",
-	Picometers: "PICOMETERS",
-	Inches: "INCHES",
-	Feet: "FEET",
-	Yards: "YARDS",
-	Miles: "MILES",
-	NauticalMiles: "NAUTICAL MILES",
-	Paces: "PACES"
+	Millimeters: 'MILLIMETERS',
+	Decimeters: 'DECIMETERS',
+	Centimeters: 'CENTIMETERS',
+	Meters: 'METERS',
+	Kilometers: 'KILOMETERS',
+	Megameters: 'MEGAMETERS',
+	Micrometers: 'MICROMETERS',
+	Nanometers: 'NANOMETERS',
+	Picometers: 'PICOMETERS',
+	Inches: 'INCHES',
+	Feet: 'FEET',
+	Yards: 'YARDS',
+	Miles: 'MILES',
+	NauticalMiles: 'NAUTICAL_MILES',
+	Paces: 'PACES',
 } as const;
 
 export const angleEnum = {
-	Degrees: "DEGREES",
-	Radians: "RADIANS",
-	Gradians: "GRADIANS",
-	Minutes: "MINUTES",
-	Seconds: "SECONDS"
+	Degrees: 'DEGREES',
+	Radians: 'RADIANS',
+	Gradians: 'GRADIANS',
+	Minutes: 'MINUTES',
+	Seconds: 'SECONDS',
 } as const;
 
 export const massEnum = {
-	Grams: "GRAMS",
-	Kilograms: "KILOGRAMS",
-	Milligrams: "MILLIGRAMS",
-	Micrograms: "MICROGRAMS",
-	Nanograms: "NANOGRAMS",
-	Pounds: "POUNDS",
-	Ounces: "OUNCES",
-	Stone: "STONE",
-	ImperialTons: "IMPERIALTONS",
-	USTons: "USTONS",
-	Tonnes: "TONNES",
-	Picograms: "PICOGRAMS",
+	Grams: 'GRAMS',
+	Kilograms: 'KILOGRAMS',
+	Milligrams: 'MILLIGRAMS',
+	Micrograms: 'MICROGRAMS',
+	Nanograms: 'NANOGRAMS',
+	Pounds: 'POUNDS',
+	Ounces: 'OUNCES',
+	Stone: 'STONE',
+	ImperialTons: 'IMPERIAL_TONS',
+	USTons: 'US_TONS',
+	Tonnes: 'TONNES',
+	Picograms: 'PICOGRAMS',
+} as const;
+
+export const volumeEnum = {
+	CubicMeters: 'CUBIC_METERS',
+	CubicFeet: 'CUBIC_FEET',
+	CubicYards: 'CUBIC_YARDS',
+	CubicInches: 'CUBIC_INCHES',
+	CubicCentimeters: 'CUBIC_CENTIMETERS',
+	CubicMillimeters: 'CUBIC_MILLIMETERS',
+	CubicMicrometers: 'CUBIC_MICROMETERS',
+	CubicNanometers: 'CUBIC_NANOMETERS',
+	CubicPicometers: 'CUBIC_PICOMETERS',
+	CubicHectometers: 'CUBIC_HECTOMETERS',
+	CubicKilometers: 'CUBIC_KILOMETERS',
+	CubicDecimeters: 'CUBIC_DECIMETERS',
+	Liters: 'LITERS',
+	Milliliters: 'MILLILITERS',
+	ImperialGallons: 'IMPERIAL_GALLONS',
+	USLiquidGallons: 'US_LIQUID_GALLONS',
+	ImperialTeaspoons: 'IMPERIAL_TEASPOONS',
+	ImperialTablespoons: 'IMPERIAL_TABLESPOONS',
+	ImperialCups: 'IMPERIAL_CUPS',
+	ImperialPints: 'IMPERIAL_PINTS',
+	ImperialQuarts: 'IMPERIAL_QUARTS',
+	USFluidQuarts: 'US_LIQUID_QUARTS',
+	ImperialFluidOunces: 'IMPERIAL_FLUID_OUNCES',
+	USTeaspoons: 'US_TEASPOONS',
+	USTablespoons: 'US_TABLESPOONS',
+	FluidOunces: 'FLUID_OUNCES',
+	USLegalCups: 'US_LEGAL_CUPS',
+	USLiqidPints: 'US_LIQUID_PINTS',
 } as const;
 
 export class Conversions {
-
 	/**
 	 * Converts a length value from one unit to another.
 	 *
@@ -55,53 +85,57 @@ export class Conversions {
 	 * @param {typeof lengthEnum | string} toUnit - The unit to convert to.
 	 * @return {number} - The converted value.
 	 */
-	static convertLength(value: number, fromUnit: typeof lengthEnum | string, toUnit: typeof lengthEnum | string): number {
+	static convertLength(
+		value: number,
+		fromUnit: typeof lengthEnum | string,
+		toUnit: typeof lengthEnum | string,
+	): number {
 		// Convert input to meters
 		let meters: number;
 		switch (fromUnit) {
-			case "MILLIMETERS":
+			case 'MILLIMETERS':
 				meters = value / 1000;
 				break;
-			case "DECIMETERS":
+			case 'DECIMETERS':
 				meters = value / 10;
 				break;
-			case "CENTIMETERS":
+			case 'CENTIMETERS':
 				meters = value / 100;
 				break;
-			case "METERS":
+			case 'METERS':
 				meters = value;
 				break;
-			case "KILOMETERS":
+			case 'KILOMETERS':
 				meters = value * 1000;
 				break;
-			case "MEGAMETERS":
+			case 'MEGAMETERS':
 				meters = value * 1000000;
 				break;
-			case "MICROMETERS":
+			case 'MICROMETERS':
 				meters = value / 1000000;
 				break;
-			case "NANOMETERS":
+			case 'NANOMETERS':
 				meters = value / 1000000000;
 				break;
-			case "PICOMETERS":
+			case 'PICOMETERS':
 				meters = value / 1000000000000;
 				break;
-			case "INCHES":
+			case 'INCHES':
 				meters = value / 39.3701;
 				break;
-			case "FEET":
+			case 'FEET':
 				meters = value / 3.28084;
 				break;
-			case "YARDS":
+			case 'YARDS':
 				meters = value / 1.09361;
 				break;
-			case "MILES":
+			case 'MILES':
 				meters = value * 1609.344;
 				break;
-			case "NAUTICAL MILES":
+			case 'NAUTICAL_MILES':
 				meters = value * 1852;
 				break;
-			case "PACES":
+			case 'PACES':
 				meters = value * 0.762;
 				break;
 			default:
@@ -111,49 +145,49 @@ export class Conversions {
 		// Convert meters to output unit
 		let output: number;
 		switch (toUnit) {
-			case "MILLIMETERS":
+			case 'MILLIMETERS':
 				output = meters * 1000;
 				break;
-			case "DECIMETERS":
+			case 'DECIMETERS':
 				output = meters * 10;
 				break;
-			case "CENTIMETERS":
+			case 'CENTIMETERS':
 				output = meters * 100;
 				break;
-			case "METERS":
+			case 'METERS':
 				output = meters;
 				break;
-			case "KILOMETERS":
+			case 'KILOMETERS':
 				output = meters / 1000;
 				break;
-			case "MEGAMETERS":
+			case 'MEGAMETERS':
 				output = meters / 1000000;
 				break;
-			case "MICROMETERS":
+			case 'MICROMETERS':
 				output = meters * 1000000;
 				break;
-			case "NANOMETERS":
+			case 'NANOMETERS':
 				output = meters * 1000000000;
 				break;
-			case "PICOMETERS":
+			case 'PICOMETERS':
 				output = meters * 1000000000000;
 				break;
-			case "INCHES":
+			case 'INCHES':
 				output = meters * 39.3701;
 				break;
-			case "FEET":
+			case 'FEET':
 				output = meters * 3.28084;
 				break;
-			case "YARDS":
+			case 'YARDS':
 				output = meters * 1.09361;
 				break;
-			case "MILES":
+			case 'MILES':
 				output = meters / 1609.344;
 				break;
-			case "NAUTICAL MILES":
+			case 'NAUTICAL_MILES':
 				output = meters / 1852;
 				break;
-			case "PACES":
+			case 'PACES':
 				output = meters / 0.762;
 				break;
 			default:
@@ -162,7 +196,6 @@ export class Conversions {
 
 		return output;
 	}
-
 
 	/**
 	 * Converts a temperature value from one unit to another.
@@ -173,17 +206,21 @@ export class Conversions {
 	 * @throws {Error} Unknown unit: fromUnit or toUnit, if the unit is not recognized.
 	 * @return {number} The temperature value converted to the desired unit.
 	 */
-	static convertTemperature(value: number, fromUnit: typeof temperatureEnum | string, toUnit: typeof temperatureEnum | string): number {
+	static convertTemperature(
+		value: number,
+		fromUnit: typeof temperatureEnum | string,
+		toUnit: typeof temperatureEnum | string,
+	): number {
 		// Convert input to Kelvin
 		let kelvin: number;
 		switch (fromUnit) {
-			case "KELVIN":
+			case 'KELVIN':
 				kelvin = value;
 				break;
-			case "CELSIUS":
+			case 'CELSIUS':
 				kelvin = value + 273.15;
 				break;
-			case "FAHRENHEIT":
+			case 'FAHRENHEIT':
 				kelvin = (value + 459.67) * (5 / 9);
 				break;
 			default:
@@ -193,13 +230,13 @@ export class Conversions {
 		// Convert Kelvin to output unit
 		let output: number;
 		switch (toUnit) {
-			case "KELVIN":
+			case 'KELVIN':
 				output = kelvin;
 				break;
-			case "CELSIUS":
+			case 'CELSIUS':
 				output = kelvin - 273.15;
 				break;
-			case "FAHRENHEIT":
+			case 'FAHRENHEIT':
 				output = kelvin * (9 / 5) - 459.67;
 				break;
 			default:
@@ -209,32 +246,35 @@ export class Conversions {
 		return output;
 	}
 
-
 	/**
-	 * Converts an angle from one unit to another.
+	 * Converts an angle value from one unit of measurement to another.
 	 *
-	 * @param {number} value - The angle to be converted.
-	 * @param {typeof angleEnum | string} fromUnit - The unit of the input angle.
-	 * @param {typeof angleEnum | string} toUnit - The unit to convert the input angle to.
-	 * @return {number} The converted angle in the desired unit.
+	 * @param {number} value - The value of the angle to be converted.
+	 * @param {typeof angleEnum | string} fromUnit - The unit of measurement of the input angle.
+	 * @param {typeof angleEnum | string} toUnit - The unit of measurement to which the angle needs to be converted.
+	 * @return {number} The value of the input angle converted to the specified unit of measurement.
 	 */
-	static convertAngle(value: number, fromUnit: typeof angleEnum | string, toUnit: typeof angleEnum | string): number {
+	static convertAngle(
+		value: number,
+		fromUnit: typeof angleEnum | string,
+		toUnit: typeof angleEnum | string,
+	): number {
 		// Convert input to degrees
 		let degrees: number;
 		switch (fromUnit) {
-			case "DEGREES":
+			case 'DEGREES':
 				degrees = value;
 				break;
-			case "RADIANS":
+			case 'RADIANS':
 				degrees = (value * 180) / Math.PI;
 				break;
-			case "GRADIANS":
-				degrees = (value * 0.9);
+			case 'GRADIANS':
+				degrees = value * 0.9;
 				break;
-			case "MINUTES":
+			case 'MINUTES':
 				degrees = value / 60;
 				break;
-			case "SECONDS":
+			case 'SECONDS':
 				degrees = value / 3600;
 				break;
 			default:
@@ -244,19 +284,19 @@ export class Conversions {
 		// Convert degrees to output unit
 		let output: number;
 		switch (toUnit) {
-			case "DEGREES":
+			case 'DEGREES':
 				output = degrees;
 				break;
-			case "RADIANS":
+			case 'RADIANS':
 				output = (degrees * Math.PI) / 180;
 				break;
-			case "GRADIANS":
-				output = (degrees / 0.9);
+			case 'GRADIANS':
+				output = degrees / 0.9;
 				break;
-			case "MINUTES":
+			case 'MINUTES':
 				output = degrees * 60;
 				break;
-			case "SECONDS":
+			case 'SECONDS':
 				output = degrees * 3600;
 				break;
 			default:
@@ -266,44 +306,56 @@ export class Conversions {
 		return output;
 	}
 
-	static convertMass(value: number, fromUnit: typeof massEnum | string, toUnit: typeof massEnum | string): number {
+	/**
+	 * Converts a mass value from one unit to another.
+	 *
+	 * @param {number} value - The value to be converted.
+	 * @param {typeof massEnum | string} fromUnit - The unit to convert from.
+	 * @param {typeof massEnum | string} toUnit - The unit to convert to.
+	 * @return {number} The converted value.
+	 */
+	static convertMass(
+		value: number,
+		fromUnit: typeof massEnum | string,
+		toUnit: typeof massEnum | string,
+	): number {
 		// Convert input to grams
 		let grams: number;
 		switch (fromUnit) {
-			case "GRAMS":
+			case 'GRAMS':
 				grams = value;
 				break;
-			case "KILOGRAMS":
+			case 'KILOGRAMS':
 				grams = value * 1000;
 				break;
-			case "MILLIGRAMS":
+			case 'MILLIGRAMS':
 				grams = grams / 1000;
 				break;
-			case "MICROGRAMS":
+			case 'MICROGRAMS':
 				grams = value / 1000000;
 				break;
-			case "NANOGRAMS":
+			case 'NANOGRAMS':
 				grams = value / 1000000000;
 				break;
-			case "PICOGRAMS":
+			case 'PICOGRAMS':
 				grams = value / 1000000000000;
 				break;
-			case "POUNDS":
+			case 'POUNDS':
 				grams = value * 453.592;
 				break;
-			case "OUNCES":
+			case 'OUNCES':
 				grams = value * 28.3495;
 				break;
-			case "USTONS":
+			case 'US_TONS':
 				grams = value * 907200;
 				break;
-			case "STONES":
+			case 'STONES':
 				grams = value * 6350.293;
 				break;
-			case "TONNES":
+			case 'TONNES':
 				grams = value * 1000000;
 				break;
-			case "IMPERIALTONS":
+			case 'IMPERIAL_TONS':
 				grams = value * 1016047;
 				break;
 			default:
@@ -313,47 +365,234 @@ export class Conversions {
 		// Convert degrees to output unit
 		let output: number;
 		switch (toUnit) {
-			case "GRAMS":
+			case 'GRAMS':
 				output = grams;
 				break;
-			case "KILOGRAMS":
+			case 'KILOGRAMS':
 				output = grams / 1000;
 				break;
-			case "TONNES":
+			case 'TONNES':
 				output = grams / 1000000;
 				break;
-			case "MILLIGRAMS":
+			case 'MILLIGRAMS':
 				output = grams * 1000;
 				break;
-			case "MICROGRAMS":
+			case 'MICROGRAMS':
 				output = grams * 1000000;
 				break;
-			case "NANOGRAMS":
+			case 'NANOGRAMS':
 				output = grams * 1000000000;
 				break;
-			case "PICOGRAMS":
+			case 'PICOGRAMS':
 				output = grams * 1000000000000;
 				break;
-			case "POUNDS":
-				output = grams/453.592;
+			case 'POUNDS':
+				output = grams / 453.592;
 				break;
-			case "OUNCES":
-				output = grams/28.3495;
+			case 'OUNCES':
+				output = grams / 28.3495;
 				break;
-			case "USTONS":
-				output = grams/907200;
+			case 'USTONS':
+				output = grams / 907200;
 				break;
-			case "STONES":
-				output = grams/6350.293;
+			case 'STONES':
+				output = grams / 6350.293;
 				break;
-			case "IMPERIALTONS":
-				output = grams/1016047;
+			case 'IMPERIAL_TONS':
+				output = grams / 1016047;
 				break;
-			
+
 			default:
 				throw new Error(`Unknown unit: ${toUnit}`);
 		}
 
+		return output;
+	}
+
+	static convertVolume(
+		value: number,
+		fromUnit: typeof volumeEnum | string,
+		toUnit: typeof volumeEnum | string,
+	): number {
+		let litres: number;
+		switch (fromUnit) {
+			case 'LITRES':
+				litres = value;
+				break;
+			case 'CUBIC_METERS':
+				litres = value * 1000;
+				break;
+			case 'CUBIC_FEET':
+				litres = value * 28.3168;
+				break;
+			case 'CUBIC_INCHES':
+				litres = value * 0.0163871;
+				break;
+			case 'CUBIC_CENTIMETERS':
+				litres = value * 0.001;
+				break;
+			case 'CUBIC_DECIMETERS':
+				litres = value;
+				break;
+			case 'CUBIC_MILLIMETERS':
+				litres = value * 0.000001;
+				break;
+			case 'CUBIC_MICROMETERS':
+				litres = value * 0.000000000000001;
+				break;
+			case 'CUBIC_NANOMETERS':
+				litres = value * 0.000000000000000000000001;
+				break;
+			case 'CUBIC_PICOMETERS':
+				litres = value * 0.000000000000000000000000000000001;
+				break;
+			case 'CUBIC_KILOMETERS':
+				litres = value * 1000000000000;
+				break;
+			case 'CUBIC_HECTOMETERS':
+				litres = value * 1000000000;
+				break;
+			case 'CUBIC_YARDS':
+				litres = value * 1760;
+				break;
+			case 'MILLILITRES':
+				litres = value * 0.001;
+				break;
+			case 'IMPERIAL_GALLONS':
+				litres = value * 4.54609;
+				break;
+			case 'IMPERIAL_PINTS':
+				litres = value * 0.568261;
+				break;
+			case 'IMPERIAL_QUARTS':
+				litres = value * 1.13652;
+				break;
+			case 'IMPERIAL_CUPS':
+				litres = value * 0.284131;
+				break;
+			case 'IMPERIAL_FLUID_OUNCE':
+				litres = value * 0.0284131;
+				break;
+			case 'IMPERIAL_TEASPOONS':
+				litres = value * 0.00591939;
+				break;
+			case 'IMPERIAL_TABLESPOONS':
+				litres = value * 0.0177582;
+				break;
+			case 'US_LIQUID_GALLONS':
+				litres = value * 3.78541;
+				break;
+			case 'US_LIQUID_PINTS':
+				litres = value * 0.473176;
+				break;
+			case 'US_LIQUID_QUARTS':
+				litres = value * 0.946353;
+				break;
+			case 'US_LEGAL_CUPS':
+				litres = value * 0.24;
+				break;
+			case 'US_TABLESPOONS':
+				litres = value * 0.0147868;
+				break;
+			case 'US_TEASPOONS':
+				litres = value * 0.00492892;
+				break;
+			case 'FLUID_OUNCE':
+				litres = value * 0.00295735;
+				break;
+			default:
+				throw new Error(`Unknown unit: ${fromUnit}`);
+		}
+
+		let output: number;
+		switch (toUnit) {
+			case 'LITRES':
+				output = litres;
+				break;
+			case 'MILLILITRES':
+				output = litres * 1000;
+				break;
+			case 'CUBIC_METERS':
+				output = litres * 0.001;
+				break;
+			case 'CUBIC_FEET':
+				output = litres * 0.0353147;
+				break;
+			case 'CUBIC_INCHES':
+				output = litres * 61.0237;
+				break;
+			case 'CUBIC_CENTIMETERS':
+				output = litres * 1000;
+				break;
+			case 'CUBIC_MILLIMETERS':
+				output = litres * 1000000;
+				break;
+			case 'CUBIC_MICROMETERS':
+				output = litres * 1000000000000000
+				break;
+			case 'CUBIC_NANOMETERS':
+				output = litres * 1000000000000000000000000;
+				break;
+			case 'CUBIC_PICOMETERS':
+				output = litres * 1000000000000000000000000000000000;
+				break;
+			case 'CUBIC_DECIMETERS':
+				output = litres;
+				break;
+			case 'CUBIC_KILOMETERS':
+				output = litres * 0.000000000001;
+				break;
+			case 'CUBIC_HECTOMETERS':
+				output = litres * 0.000000001;
+				break;
+			case 'CUBIC_YARDS':
+				output = litres * 0.00130795;
+				break;
+			case 'IMPERIAL_GALLONS':
+				output = litres * 4.54609;
+				break;
+			case 'IMPERIAL_PINTS':
+				output = litres * 1.75975;
+				break;
+			case 'IMPERIAL_QUARTS':
+				output = litres * 0.879877;
+				break;
+			case 'IMPERIAL_CUPS':
+				output = litres * 3.51951;
+				break;
+			case 'IMPERIAL_FLUID_OUNCES':
+				output = litres * 35.1951;
+				break;
+			case 'IMPERIAL_TEASPOONS':
+				output = litres * 168.936;
+				break;
+			case 'IMPERIAL_TABLESPOONS':
+				output = litres * 56.3121;
+				break;
+			case 'US_LIQUID_GALLONS':
+				output = litres * 0.264172;
+				break;
+			case 'US_LIQUID_PINTS':
+				output = litres * 2.11338;
+				break;
+			case 'US_LIQUID_QUARTS':
+				output = litres * 1.05669;
+				break;
+			case 'US_LEGAL_CUPS':
+				output = litres * 4.16667;
+				break;
+			case 'US_TABLESPOONS':
+				output = litres * 67.628;
+				break;
+			case 'US_TEASPOONS':
+				output = litres * 0.00492892;
+				break;
+			case 'FLUID_OUNCE':
+				output = litres * 33.814;
+				break;
+			default:
+				throw new Error(`Unknown unit: ${toUnit}`);
+		}
 		return output;
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './simple/Fraction';
 export * from './util';
 export * from './2dshapes/circle';
 export * from './matrices/Matrices';
+export * from './conversions/Conversions';
