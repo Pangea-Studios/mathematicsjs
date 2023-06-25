415bdcd1d33d10674f424cc9028f0e40309b273c
PokyBread <luksel09@gmail.com>
2023-06-25 08:23:01 +0100

    Update Constants.ts

--- src/units/Constants.ts
+++ src/units/Constants.ts
@@ -63,129 +63,129 @@ export const NumberTypes = {
  */
 
 export const MathsConstants = {
-	e: { value: 2.71828_18284_59045_23536, type: NumberTypes.Transcendental },
+	e: { value: 2.71828_18284_59045, type: NumberTypes.Transcendental },
 	eulersNumber: {
-		value: 2.71828_18284_59045_23536,
+		value: 2.71828_18284_59045,
 		type: NumberTypes.Transcendental,
 	},
 
-	pi: { value: 3.14159_26535_89793_23846, type: NumberTypes.Transcendental },
-	π: { value: 3.14159_26535_89793_23846, type: NumberTypes.Transcendental },
+	pi: { value: 3.14159_26535_89793, type: NumberTypes.Transcendental },
+	π: { value: 3.14159_26535_89793, type: NumberTypes.Transcendental },
 
 	ε: 0.0001,
 	epsilon: 0.0001,
 
-	τ: { value: 6.28318_53071_79586_47692, type: NumberTypes.Transcendental },
-	tau: { value: 6.28318_53071_79586_47692, type: NumberTypes.Transcendental },
+	τ: { value: 6.28318_53071_79586, type: NumberTypes.Transcendental },
+	tau: { value: 6.28318_53071_79586, type: NumberTypes.Transcendental },
 
-	ϕ: { value: 1.61803_39887_49894_84820, type: NumberTypes.Algebraic },
-	phi: { value: 1.61803_39887_49894_84820, type: NumberTypes.Algebraic },
+	ϕ: { value: 1.61803_39887_49894, type: NumberTypes.Algebraic },
+	phi: { value: 1.61803_39887_49894, type: NumberTypes.Algebraic },
 	goldenRatio: {
-		value: 1.61803_39887_49894_84820,
+		value: 1.61803_39887_49894,
 		type: NumberTypes.Algebraic,
 	},
-	φ: { value: 1.61803_39887_49894_84820, type: NumberTypes.Algebraic },
+	φ: { value: 1.61803_39887_49894, type: NumberTypes.Algebraic },
 
-	sqrt2: { value: 1.41421_35623_73095_04880, type: NumberTypes.Algebraic },
-	sqrt3: { value: 1.73205_08075_68877_29352, type: NumberTypes.Algebraic },
-	sqrt5: { value: 2.23606_79774_99789_69640, type: NumberTypes.Algebraic },
+	sqrt2: { value: 1.41421_35623_73095, type: NumberTypes.Algebraic },
+	sqrt3: { value: 1.73205_08075_68877, type: NumberTypes.Algebraic },
+	sqrt5: { value: 2.23606_79774_99789, type: NumberTypes.Algebraic },
 
 	silverRatio: {
-		value: 2.41421_35623_73095_04880,
+		value: 2.41421_35623_73095,
 		type: NumberTypes.Algebraic,
 	},
-	δS: { value: 2.41421_35623_73095_04880, type: NumberTypes.Algebraic },
+	δS: { value: 2.41421_35623_73095, type: NumberTypes.Algebraic },
 
 	cubeRoot2: {
-		value: 1.25992_10498_94873_16476,
+		value: 1.25992_10498_94873,
 		type: NumberTypes.Algebraic,
 	},
 	cubeRoot3: {
-		value: 1.44224_95703_07408_38232,
+		value: 1.44224_95703_07408,
 		type: NumberTypes.Algebraic,
 	},
 	twelthRoot2: {
-		value: 1.05946_30943_59295_26456,
+		value: 1.05946_30943_59295,
 		type: NumberTypes.Algebraic,
 	},
 
 	supergoldenRatio: {
-		value: 1.46557_12318_76768_02665,
+		value: 1.46557_12318_76768,
 		type: NumberTypes.Algebraic,
 	},
-	ψ: { value: 1.46557_12318_76768_02665, type: NumberTypes.Algebraic },
+	ψ: { value: 1.46557_12318_76768, type: NumberTypes.Algebraic },
 
-	µ0: { value: 1.84775_90650_22573_51225, type: NumberTypes.Algebraic },
+	µ0: { value: 1.84775_90650_22573, type: NumberTypes.Algebraic },
 	hexagonalLatticeConnectiveConstant: {
-		value: 1.84775_90650_22573_51225,
+		value: 1.84775_90650_22573,
 		type: NumberTypes.Algebraic,
 	},
 
 	keplerBouwkampConstant: {
-		value: 0.11494_20448_53296_20070,
+		value: 0.11494_20448_53296,
 		type: NumberTypes.Real,
 	},
-	K: { value: 0.11494_20448_53296_20070, type: NumberTypes.Real },
+	K: { value: 0.11494_20448_53296, type: NumberTypes.Real },
 
 	WallisConstant: {
-		value: 0.11494_20448_53296_59148,
+		value: 0.11494_20448_53296,
 		type: NumberTypes.Algebraic,
 	},
 
-	ln2: { value: 0.69314_71805_59945_30941, type: NumberTypes.Transcendental },
+	ln2: { value: 0.69314_71805_59945, type: NumberTypes.Transcendental },
 
 	lemniscateConstant: {
-		value: 2.62205_75542_92119_81046,
+		value: 2.62205_75542_92119,
 		type: NumberTypes.Transcendental,
 	},
-	ϖ: { value: 2.62205_75542_92119_81046, type: NumberTypes.Transcendental },
+	ϖ: { value: 2.62205_75542_92119, type: NumberTypes.Transcendental },
 
 	eulersConstant: {
-		value: 0.57721_56649_01532_86060,
+		value: 0.57721_56649_01532,
 		type: NumberTypes.Real,
 	},
-	γ: { value: 0.57721_56649_01532_86060, type: NumberTypes.Real },
+	γ: { value: 0.57721_56649_01532, type: NumberTypes.Real },
 
 	erdősBorweinConstant: {
-		value: 1.60669_51524_15291_76378,
+		value: 1.60669_51524_15291,
 		type: NumberTypes.Irrational,
 	},
-	E: { value: 1.60669_51524_15291_76378, type: NumberTypes.Irrational },
+	E: { value: 1.60669_51524_15291, type: NumberTypes.Irrational },
 
 	omegaConstant: {
-		value: 0.56714_32904_09783_87299,
+		value: 0.56714_32904_09783,
 		type: NumberTypes.Transcendental,
 	},
-	Ω: { value: 0.56714_32904_09783_87299, type: NumberTypes.Transcendental },
+	Ω: { value: 0.56714_32904_09783, type: NumberTypes.Transcendental },
 
 	apérysConstant: {
-		value: 1.20205_69031_59594_28539,
+		value: 1.20205_69031_59594,
 		type: NumberTypes.Irrational,
 	},
-	'ζ(3)': { value: 1.20205_69031_59594_28539, type: NumberTypes.Irrational },
+	'ζ(3)': { value: 1.20205_69031_59594, type: NumberTypes.Irrational },
 
 	laplaceLimit: {
-		value: 1.20205_69031_59594_28539,
+		value: 1.20205_69031_59594,
 		type: NumberTypes.Transcendental,
 	},
 
 	RamanujanSoldnerConstant: {
-		value: 1.45136_92348_83381_05028,
+		value: 1.45136_92348_83381,
 		type: NumberTypes.Real,
 	},
-	μ1: { value: 1.45136_92348_83381_05028, type: NumberTypes.Real },
+	μ1: { value: 1.45136_92348_83381, type: NumberTypes.Real },
 
 	GausssConstant: {
-		value: 1.45136_92348_83381_05028,
+		value: 1.45136_92348_83381,
 		type: NumberTypes.Transcendental,
 	},
-	G0: { value: 1.45136_92348_83381_05028, type: NumberTypes.Transcendental },
+	G0: { value: 1.45136_92348_83381, type: NumberTypes.Transcendental },
 
 	secondHermiteConstant: {
-		value: 1.15470_05383_79251_52901,
+		value: 1.15470_05383_79251,
 		type: NumberTypes.Algebraic,
 	},
-	γ2: { value: 1.15470_05383_79251_52901, type: NumberTypes.Algebraic },
+	γ2: { value: 1.15470_05383_79251, type: NumberTypes.Algebraic },
 
 	liouvillesConstant: {
 		value: 0.11000_10000_00000_00000_0001,
@@ -197,23 +197,70 @@ export const MathsConstants = {
 	},
 
 	firstContinuedFractionConstant: {
-		value: 0.69777_46579_64007_98201,
+		value: 0.69777_46579_64007,
 		type: NumberTypes.Irrational,
 	},
-	C1: { value: 0.69777_46579_64007_98201, type: NumberTypes.Irrational },
+	C1: { value: 0.69777_46579_64007, type: NumberTypes.Irrational },
 
 	RamanujansConstant: {
-		value: 262_53741_26407_68743.99999_99999_99250_073,
+		value: 262_53741_26407_68743.99999_99999_99250,
 		type: NumberTypes.Transcendental,
 	},
 
-	glaisherKinkelinConstant: { value: 1.28242_71291_00622_63687, type: NumberTypes.Real },
-	A: { value: 1.28242_71291_00622_63687, type: NumberTypes.Real },
+	glaisherKinkelinConstant: {
+		value: 1.28242_71291_00622,
+		type: NumberTypes.Real,
+	},
+	A: { value: 1.28242_71291_00622, type: NumberTypes.Real },
 
-	catalansConstant: { value: 0.91596_55941_77219_01505, type: NumberTypes.Real },
-	G1: { value: 0.91596_55941_77219_01505, type: NumberTypes.Real },
+	catalansConstant: { value: 0.91596_55941_77219, type: NumberTypes.Real },
+	G1: { value: 0.91596_55941_77219, type: NumberTypes.Real },
 
-	dottieNumber: {value: 0.73908_51332_15160_64165, type: NumberTypes.Transcendental},
+	dottieNumber: {
+		value: 0.73908_51332_15160,
+		type: NumberTypes.Transcendental,
+	},
 
-	
+	meisselMertensConstant: {
+		value: 0.26149_72128_47642,
+		type: NumberTypes.Real,
+	},
+	M: { value: 0.26149_72128_47642, type: NumberTypes.Real },
+
+	univeralParabolicConstant: {
+		value: 2.29558_71493_92638,
+		type: NumberTypes.Transcendental,
+	},
+	P: { value: 2.29558_71493_92638, type: NumberTypes.Transcendental },
+
+	cahensConstant: {
+		value: 0.64341_05462_88338,
+		type: NumberTypes.Transcendental,
+	},
+	C: { value: 0.64341_05462_88338, type: NumberTypes.Transcendental },
+
+	gelfondsConstant: {
+		value: 23.14069_26327_792,
+		type: NumberTypes.Transcendental,
+	},
+	α: { value: 23.14069_26327_792, type: NumberTypes.Transcendental },
+	e_pi: { value: 23.14069_26327_792, type: NumberTypes.Transcendental },
+
+	gelfondSchneiderConstant: {
+		value: 2.66514_41426_90225,
+		type: NumberTypes.Transcendental,
+	},
+	two_sqrt2: { value: 2.66514_41426_90225, type: NumberTypes.Transcendental },
+
+	secondFavardConstant: {
+		value: 1.23370_05501_36169,
+		type: NumberTypes.Transcendental,
+	},
+	K2: { value: 1.23370_05501_36169, type: NumberTypes.Transcendental },
+
+	goldenAngle: {
+		value: 2.39996_32297_28653,
+		type: NumberTypes.Transcendental,
+	},
+	g: { value: 2.39996_32297_28653, type: NumberTypes.Transcendental },
 } as const;
