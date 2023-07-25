44f90e3b3463210b7e1e6935eb122098701f3e09
PokyBread <luksel09@gmail.com>
2023-07-25 13:34:27 +0100

    Update Triangle.ts

--- src/2dshapes/Triangle.ts
+++ src/2dshapes/Triangle.ts
@@ -17,19 +17,37 @@ export class RightAngledTriangle {
 		if (hypotenuse === null) {
 			if (sideA && sideB) {
 				this.hypotenuse = (sideA * sideA + sideB * sideB) ** 0.5;
+			} else if (sideA && angleA) {
+				this.hypotenuse= (sideA/Trig.cos(angleA))
+			} else if (sideB && angleB) {
+				this.hypotenuse = (sideB/Trig.cos(angleB))
+			} else if (sideA && angleB) {
+				this.hypotenuse = (sideA/Trig.sin(angleB))
+			} else if (sideB && angleA) {
+				this.hypotenuse = (sideB/Trig.sin(angleA))
 			}
+		} else {
+			this.hypotenuse = hypotenuse;
 		}
 
 		if (sideA === null) {
 			if (sideB && hypotenuse) {
 				sideA = (hypotenuse * hypotenuse - sideB * sideB) ** 0.5;
+			} else if (angleA && hypotenuse) {
+				sideA = hypotenuse * Trig.cos(angleA)
+			} else if (angleB && hypotenuse) {
+				sideA = hypotenuse * Trig.cos(angleB)
 			}
+		} else {
+			this.sideA = sideA;
 		}
 
 		if (sideB === null) {
 			if (sideA && hypotenuse) {
 				sideB = (hypotenuse * hypotenuse - sideA * sideA) ** 0.5;
 			}
+		} else {
+			this.sideB = sideB;
 		}
 	}
 }
